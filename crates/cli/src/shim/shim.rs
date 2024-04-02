fc176328bb705d1243aaad747c429fa64c67cf91
ityuany <519495771@qq.com>
2024-04-03 01:23:32 +0800

    review shim

--- crates/cli/src/shim/shim.rs
+++ crates/cli/src/shim/shim.rs
@@ -1,8 +1,14 @@
-use std::process::Output;
+use std::{env::current_dir, process::Output};
 
-use snm_core::{exec_child_process, model::SnmError, utils::health::check_multi_lock_file};
+use snm_core::{
+    exec_child_process,
+    model::{PackageJson, SnmError},
+    utils::health::check_multi_lock_file,
+};
 use snm_node::node_mg::use_node;
+use snm_npm::snm_npm::{SnmNpm, SnmNpmTrait};
 use snm_pm::get_manager_bin_file_path;
+use snm_yarn::snm_yarn::SnmYarn;
 
 pub async fn launch(name: &str) -> Result<Output, SnmError> {
     env_logger::init();
@@ -11,31 +17,43 @@ pub async fn launch(name: &str) -> Result<Output, SnmError> {
 
     check_multi_lock_file()?;
 
+    let pkg_file_path = current_dir()?.join("package.json");
+
+    let package_json = PackageJson::from_file_path(Some(pkg_file_path))?;
+
+    // TODO parse_package_manager 可能需要返回 None 不能直接报错
+    let package_manager = package_json.parse_package_manager()?;
+
     let res = match name {
-        "yarn" | "npm" | "pnpm" => launch_package_manager_shim(name).await?,
-        "node" => launch_node_shim(name).await?,
+        "npm" => {
+            let snm_npm = SnmNpm::new(None);
+            let bin_file_path = snm_npm.use_bin("npm", None).await?;
+            let args: Vec<String> = std::env::args().skip(1).collect();
+            let output = exec_child_process!(bin_file_path, &args)?;
+            output
+        }
+        "pnpm" => {
+            let snm_npm = SnmNpm::new(None);
+            let bin_file_path = snm_npm.use_bin("pnpm", None).await?;
+            let args: Vec<String> = std::env::args().skip(1).collect();
+            let output = exec_child_process!(bin_file_path, &args)?;
+            output
+        }
+        "yarn" => {
+            let snm_yarn = SnmYarn::new();
+            let bin_file_path = snm_yarn.use_bin("yarn", None).await?;
+            let args: Vec<String> = std::env::args().skip(1).collect();
+            let output = exec_child_process!(bin_file_path, &args)?;
+            output
+        }
+        "node" => {
+            let node_binary_abs_path = use_node().await?;
+            let args: Vec<String> = std::env::args().skip(1).collect();
+            let output = exec_child_process!(node_binary_abs_path, &args)?;
+            output
+        }
         _ => return Err(SnmError::UnknownError),
     };
 
     Ok(res)
-}
-
-async fn launch_package_manager_shim(name: &str) -> Result<Output, SnmError> {
-    let bin_file_path = get_manager_bin_file_path(name).await?;
-
-    let args: Vec<String> = std::env::args().skip(1).collect();
-
-    let output = exec_child_process!(bin_file_path, &args)?;
-
-    Ok(output)
-}
-
-async fn launch_node_shim(_name: &str) -> Result<Output, SnmError> {
-    let node_binary_abs_path = use_node().await?;
-
-    let args: Vec<String> = std::env::args().skip(1).collect();
-
-    let output = exec_child_process!(node_binary_abs_path, &args)?;
-
-    Ok(output)
 }
