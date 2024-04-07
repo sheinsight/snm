f578bb67ede1a4d62b777cadd135d8b77d3ed91e
ityuany <519495771@qq.com>
2024-04-08 01:43:50 +0800

    🤔 i think the logic should write into entry rs, manager crates should power atom api

--- crates/cli/src/shim/node.rs
+++ crates/cli/src/shim/node.rs
@@ -1,11 +1,11 @@
 use colored::*;
 use snm_core::{
-    config::SnmConfig,
+    config::{snm_config::InstallStrategy, SnmConfig},
     exec_proxy_child_process,
     model::{snm_error::handle_snm_error, SnmError},
     println_success,
 };
-use snm_node::node_mg::{use_bin, use_default_node};
+use snm_node::node_mg::{ask_download, download};
 use std::{env::current_dir, fs::read_to_string, ops::Not, process::Output};
 
 const NODE_VERSION_FILE: &str = ".node-version";
@@ -14,46 +14,52 @@ const NODE_VERSION_FILE: &str = ".node-version";
 async fn main() {
     env_logger::init();
 
-    let snm_config = SnmConfig::new();
-
-    if let Err(error) = if snm_config.get_strict() {
-        execute_strict().await
-    } else {
-        execute().await
-    } {
+    if let Err(error) = execute().await {
         handle_snm_error(error);
     }
 }
 
 async fn execute() -> Result<Output, SnmError> {
-    let node_version_path_buf = current_dir()?.join(NODE_VERSION_FILE);
-    if node_version_path_buf.exists().not() {
-        let mut stdout = std::io::stdout();
-        let (v, bin_path_buf) = use_default_node().await?;
-        println_success!(
-            stdout,
-            "Use Node {} . {}",
-            format!("{}", v.green()),
-            "by default".bright_black()
-        );
-        Ok(exec_proxy_child_process!(&bin_path_buf)?)
-    } else {
-        Ok(execute_strict().await?)
-    }
-}
+    let snm_config = SnmConfig::new();
 
-async fn execute_strict() -> Result<Output, SnmError> {
-    let mut stdout = std::io::stdout();
     let node_version_path_buf = current_dir()?.join(NODE_VERSION_FILE);
 
     if node_version_path_buf.exists().not() {
-        return Err(SnmError::NotFoundNodeVersionFileError {
-            file_path: node_version_path_buf.display().to_string(),
-        });
+        if snm_config.get_strict() {
+            Err(SnmError::NotFoundNodeVersionFileError {
+                file_path: node_version_path_buf.display().to_string(),
+            })?;
+        } else {
+            todo!("use default node")
+        }
     }
-    let v = read_to_string(&node_version_path_buf)
-        .map(|value| value.trim_start_matches(['v', 'V']).trim().to_string())?;
-    let (v, bin_path_buf) = use_bin(&v).await?;
-    println_success!(stdout, "Use Node {} .", format!("{}", v.green()));
-    Ok(exec_proxy_child_process!(&bin_path_buf)?)
+
+    let version_processor = |value: String| value.trim_start_matches(['v', 'V']).trim().to_string();
+    let v = read_to_string(&node_version_path_buf).map(version_processor)?;
+
+    let node_binary_path_buf = snm_config
+        .get_node_bin_dir_path_buf()?
+        .join(&v)
+        .join("bin")
+        .join("node");
+
+    if node_binary_path_buf.exists().not() {
+        match snm_config.get_node_install_strategy()? {
+            InstallStrategy::Ask => {
+                if ask_download(&v)? {
+                    download(&v).await?;
+                }
+            }
+            InstallStrategy::Install => {
+                download(&v).await?;
+            }
+            InstallStrategy::Panic => {
+                return Err(SnmError::UnSupportNodeVersion { version: v });
+            }
+        }
+    }
+
+    println_success!(std::io::stdout(), "Use Node {}. ", v.green());
+
+    Ok(exec_proxy_child_process!(&node_binary_path_buf)?)
 }
