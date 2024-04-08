301df06b506f2768650253855d0ec8198646267d
ityuany <519495771@qq.com>
2024-04-08 22:34:21 +0800

    add common behavior

---
+++ crates/snm_core/src/model/common_behavior.rs
@@ -0,0 +1,3 @@
+pub struct CommonBehavior {}
+
+impl CommonBehavior {}
--- crates/snm_core/src/model/mod.rs
+++ crates/snm_core/src/model/mod.rs
@@ -5,6 +5,7 @@ pub use self::package_json_model::Bin;
 pub use self::package_json_model::PackageJson;
 pub use self::snm_error::SnmError;
 
+pub mod common_behavior;
 pub mod node_model;
 pub mod node_schedule_model;
 pub mod package_json_model;
