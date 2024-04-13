c7f77d1c5bd334268446689b1d2774083c54e5d4
ityuany <519495771@qq.com>
2024-04-14 02:04:47 +0800

    remove unused fn

--- crates/snm_core/src/utils/abs_to_user.rs
+++
@@ -1,8 +0,0 @@
-use std::path::PathBuf;
-
-pub fn transform_abs_to_user(abs_path: &PathBuf) -> String {
-    let home_dir = dirs::home_dir().expect("Failed to get home dir");
-    let home_dir_str = home_dir.to_str().unwrap();
-    let abs_path_str = abs_path.display().to_string();
-    abs_path_str.replace(home_dir_str, "~")
-}
