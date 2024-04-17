7858c61704c12562b63160f1d74932041fa3a3c9
ityuany <519495771@qq.com>
2024-04-17 15:14:57 +0800

    Refactor

---
+++ crates/cli/src/fig.rs
@@ -0,0 +1,39 @@
+use std::{fs, ops::Not};
+
+use clap::CommandFactory;
+use snm_core::{model::SnmError, println_success};
+
+use crate::SnmCli;
+
+pub fn fig_spec_impl() -> Result<(), SnmError> {
+    let mut output = Vec::new();
+    clap_complete::generate(
+        clap_complete_fig::Fig,
+        &mut SnmCli::command(),
+        "snm",
+        &mut output,
+    );
+    let output_string = String::from_utf8(output).unwrap();
+
+    if let Some(home) = dirs::home_dir() {
+        let dir = home.join(".fig").join("autocomplete").join("build");
+
+        if dir.exists().not() {
+            fs::create_dir_all(&dir)?;
+        }
+
+        let spec_path_buf = dir.join("snm.ts");
+
+        if spec_path_buf.exists() {
+            fs::remove_file(&spec_path_buf)?;
+        }
+
+        fs::write(&spec_path_buf, &output_string)?;
+
+        println_success!(
+            "Fig spec file has been created at {}",
+            spec_path_buf.display()
+        );
+    }
+    Ok(())
+}
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,6 +1,7 @@
+use bump::bump_impl;
 use clap::{command, CommandFactory, Parser};
 use colored::*;
-use dialoguer::{theme::ColorfulTheme, Select};
+use fig::fig_spec_impl;
 use manage_command::ManageCommands;
 use ni::{
     npm_args::NpmArgsTransform,
@@ -9,9 +10,7 @@ use ni::{
     yarn_args::YarnArgsTransform,
     yarnpkg_args::YarnPkgArgsTransform,
 };
-use regex::Regex;
-use semver::{Prerelease, Version};
-use serde_json::Value;
+use semver::Version;
 use snm_command::SnmCommands;
 use snm_core::{
     config::SnmConfig,
@@ -26,11 +25,13 @@ use snm_npm::snm_npm::SnmNpm;
 use snm_pnpm::snm_pnpm::SnmPnpm;
 use snm_yarn::{snm_yarn::SnmYarn, snm_yarnpkg::SnmYarnPkg};
 use std::{
-    env::current_dir,
     fs,
     ops::Not,
+    path::PathBuf,
     process::{Command, Stdio},
 };
+mod bump;
+mod fig;
 mod manage_command;
 mod ni;
 mod snm_command;
@@ -54,21 +55,130 @@ async fn main() -> Result<(), SnmError> {
 
 async fn execute_cli() -> Result<(), SnmError> {
     let cli = SnmCli::parse();
-
+    let trim_version = |version: String| version.trim_start_matches(['v', 'V']).trim().to_owned();
     match cli.command {
         // manage start
-        SnmCommands::Yarn { command } => {
-            exec_manage(command, Box::new(SnmYarn::new())).await?;
-        }
-        SnmCommands::Pnpm { command } => {
-            exec_manage(command, Box::new(SnmPnpm::new())).await?;
-        }
-        SnmCommands::Npm { command } => {
-            exec_manage(command, Box::new(SnmNpm::new())).await?;
-        }
-        SnmCommands::Node { command } => {
-            exec_manage(command, Box::new(SnmNode::new())).await?;
-        }
+        SnmCommands::Yarn { command } => match command {
+            ManageCommands::Default { version } => {
+                let v: &String = &trim_version(version);
+                let manager: Box<dyn ManageTrait> = if get_is_less_2(&v)? {
+                    Box::new(SnmYarn::new())
+                } else {
+                    Box::new(SnmYarnPkg::new())
+                };
+                DispatchManage::new(manager).set_default(v).await?;
+            }
+            ManageCommands::Install { version } => {
+                let v: &String = &trim_version(version);
+                let manager: Box<dyn ManageTrait> = if get_is_less_2(&v)? {
+                    Box::new(SnmYarn::new())
+                } else {
+                    Box::new(SnmYarnPkg::new())
+                };
+                DispatchManage::new(manager).install(v).await?;
+            }
+            ManageCommands::Uninstall { version } => {
+                let v: &String = &trim_version(version);
+                let manager: Box<dyn ManageTrait> = if get_is_less_2(&v)? {
+                    Box::new(SnmYarn::new())
+                } else {
+                    Box::new(SnmYarnPkg::new())
+                };
+                DispatchManage::new(manager).un_install(v).await?;
+            }
+            ManageCommands::List => {
+                DispatchManage::new(Box::new(SnmYarn::new())).list().await?;
+            }
+            ManageCommands::ListRemote { all } => {
+                DispatchManage::new(Box::new(SnmYarn::new()))
+                    .list_remote(all)
+                    .await?;
+            }
+        },
+        SnmCommands::Pnpm { command } => match command {
+            ManageCommands::Default { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmPnpm::new()))
+                    .set_default(v)
+                    .await?
+            }
+            ManageCommands::Install { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmPnpm::new()))
+                    .install(v)
+                    .await?;
+            }
+            ManageCommands::Uninstall { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmPnpm::new()))
+                    .un_install(v)
+                    .await?;
+            }
+            ManageCommands::List => {
+                DispatchManage::new(Box::new(SnmPnpm::new())).list().await?;
+            }
+            ManageCommands::ListRemote { all } => {
+                DispatchManage::new(Box::new(SnmPnpm::new()))
+                    .list_remote(all)
+                    .await?;
+            }
+        },
+        SnmCommands::Npm { command } => match command {
+            ManageCommands::Default { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmNpm::new()))
+                    .set_default(v)
+                    .await?
+            }
+            ManageCommands::Install { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmNpm::new()))
+                    .install(v)
+                    .await?;
+            }
+            ManageCommands::Uninstall { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmNpm::new()))
+                    .un_install(v)
+                    .await?;
+            }
+            ManageCommands::List => {
+                DispatchManage::new(Box::new(SnmNpm::new())).list().await?;
+            }
+            ManageCommands::ListRemote { all } => {
+                DispatchManage::new(Box::new(SnmNpm::new()))
+                    .list_remote(all)
+                    .await?;
+            }
+        },
+        SnmCommands::Node { command } => match command {
+            ManageCommands::Default { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmNode::new()))
+                    .set_default(v)
+                    .await?
+            }
+            ManageCommands::Install { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmNode::new()))
+                    .install(v)
+                    .await?;
+            }
+            ManageCommands::Uninstall { version } => {
+                let v: &String = &trim_version(version);
+                DispatchManage::new(Box::new(SnmNode::new()))
+                    .un_install(v)
+                    .await?;
+            }
+            ManageCommands::List => {
+                DispatchManage::new(Box::new(SnmNode::new())).list().await?;
+            }
+            ManageCommands::ListRemote { all } => {
+                DispatchManage::new(Box::new(SnmNode::new()))
+                    .list_remote(all)
+                    .await?;
+            }
+        },
         // manage end
 
         // snm command start
@@ -92,177 +202,44 @@ async fn execute_cli() -> Result<(), SnmError> {
         SnmCommands::Query => todo!(),
         // snm command end
         SnmCommands::FigSpec => {
-            let mut output = Vec::new();
-            clap_complete::generate(
-                clap_complete_fig::Fig,
-                &mut SnmCli::command(),
-                "snm",
-                &mut output,
-            );
-            let output_string = String::from_utf8(output).unwrap();
-
-            if let Some(home) = dirs::home_dir() {
-                let dir = home.join(".fig").join("autocomplete").join("build");
-
-                if dir.exists().not() {
-                    fs::create_dir_all(&dir)?;
-                }
-
-                let spec_path_buf = dir.join("snm.ts");
-
-                if spec_path_buf.exists() {
-                    fs::remove_file(&spec_path_buf)?;
-                }
-
-                fs::write(&spec_path_buf, &output_string)?;
-
-                println_success!(
-                    "Fig spec file has been created at {}",
-                    spec_path_buf.display()
-                );
-            }
+            fig_spec_impl()?;
         }
         SnmCommands::Bump => {
-            let package_json = PackageJson::from_dir_path(None)?;
-            let current_version =
-                Version::parse(package_json.version.unwrap_or("0.0.0".to_string()).as_str())?;
-            let prerelease_number = current_version.pre.parse::<u8>().unwrap_or(0) + 1;
-
-            let major = current_version.major;
-            let minor = current_version.minor;
-            let patch = current_version.patch;
-
-            let versions_and_strings = vec![
-                create_version_and_string("major", major + 1, 0, 0, None)?,
-                create_version_and_string("minor", major, minor + 1, 0, None)?,
-                create_version_and_string("patch", major, minor, patch + 1, None)?,
-                create_version_and_string(
-                    "premajor",
-                    major + 1,
-                    0,
-                    0,
-                    Some(Prerelease::new("0")?),
-                )?,
-                create_version_and_string(
-                    "preminor",
-                    major,
-                    minor + 1,
-                    0,
-                    Some(Prerelease::new("0")?),
-                )?,
-                create_version_and_string(
-                    "prepatch",
-                    major,
-                    minor,
-                    patch + 1,
-                    Some(Prerelease::new("0")?),
-                )?,
-                create_version_and_string(
-                    "prerelease",
-                    major,
-                    minor,
-                    patch,
-                    Some(Prerelease::new(prerelease_number.to_string().as_str())?),
-                )?,
-            ];
-
-            let selections: Vec<String> = versions_and_strings
-                .iter()
-                .map(|(_, s)| s.clone())
-                .collect();
-            let selection = Select::with_theme(&ColorfulTheme::default())
-                .with_prompt(format!(
-                    "请选择要升级的版本号: {} ",
-                    current_version.to_string().bright_purple()
-                ))
-                .default(0)
-                .items(&selections[..])
-                .interact()?;
-
-            let dir = current_dir()?;
-
-            let c = fs::read_to_string(dir.join("package.json"))?;
-
-            let version_regex = Regex::new(r#""version"\s*:\s*"[^"]*""#)?;
-            let replacement = format!(
-                r#""version": "{}""#,
-                versions_and_strings[selection].0.to_string()
-            );
-
-            let x = version_regex.replace(&c, replacement.as_str());
-
-            fs::write(dir.join("package.json"), x.to_string())?;
-
-            println!(
-                "您选择了: {} , {:?}",
-                selections[selection], versions_and_strings[selection].0
-            );
+            bump_impl()?;
         }
     }
     Ok(())
 }
 
-fn create_version_and_string(
-    version_type: &str,
-    major: u64,
-    minor: u64,
-    patch: u64,
-    pre: Option<Prerelease>,
-) -> Result<(Version, String), SnmError> {
-    let mut new_version = Version::new(major, minor, patch);
-    if let Some(p) = pre {
-        new_version.pre = p.clone();
-    }
-    let version_string = format!(
-        "{:<12} {}",
-        version_type,
-        new_version.to_string().bright_black()
-    );
-    Ok((new_version, version_string))
-}
-
-pub async fn exec_manage(
-    command: ManageCommands,
-    manager: Box<dyn ManageTrait>,
-) -> Result<(), SnmError> {
+async fn get_bin() -> Result<(PackageManager, PathBuf), SnmError> {
+    let package_manager = PackageJson::from_dir_path(None)?.parse_package_manager()?;
+    let manager = get_manage(&package_manager).await?;
     let dispatcher = DispatchManage::new(manager);
-
-    let trim_version = |version: String| version.trim_start_matches(['v', 'V']).trim().to_owned();
-
-    match command {
-        ManageCommands::Default { version } => {
-            dispatcher.set_default(&trim_version(version)).await?
-        }
-        ManageCommands::Install { version } => {
-            dispatcher.install(&trim_version(version)).await?;
-        }
-        ManageCommands::Uninstall { version } => {
-            dispatcher.un_install(&trim_version(version)).await?;
-        }
-        ManageCommands::List => {
-            dispatcher.list().await?;
-        }
-        ManageCommands::ListRemote { all } => {
-            dispatcher.list_remote(all).await?;
-        }
-    };
-    Ok(())
+    let (_, bin_path_buf) = dispatcher.ensure_strict_package_manager().await?;
+    Ok((package_manager, bin_path_buf))
 }
 
 async fn execute_command<F>(get_command_args: F) -> Result<(), SnmError>
 where
     F: FnOnce(&dyn CommandArgsCreatorTrait) -> Result<Vec<String>, SnmError>,
 {
-    let package_manager = get_package_manager()?;
+    let (package_manager, bin_path_buf) = get_bin().await?;
 
-    let manager = get_manage(&package_manager).await?;
-
-    let command_args_creator = get_command_args_creator(&package_manager)?;
+    let command_args_creator: Box<dyn CommandArgsCreatorTrait> = match package_manager.name.as_str()
+    {
+        "npm" => Box::new(NpmArgsTransform {}),
+        "pnpm" => Box::new(PnpmArgsTransform {}),
+        "yarn" => {
+            if get_is_less_2(&package_manager.version)? {
+                Box::new(YarnArgsTransform {})
+            } else {
+                Box::new(YarnPkgArgsTransform {})
+            }
+        }
+        _ => panic!("Unsupported package manager"),
+    };
 
     let args = get_command_args(command_args_creator.as_ref())?;
-
-    let dispatcher = DispatchManage::new(manager);
-    let (_, bin_path_buf) = dispatcher.ensure_strict_package_manager().await?;
 
     println_success!(
         "Use {}. {}",
@@ -278,29 +255,6 @@ where
         .spawn()
         .and_then(|process| process.wait_with_output())?;
     Ok(())
-}
-
-fn get_package_manager() -> Result<PackageManager, SnmError> {
-    let package_json = PackageJson::from_dir_path(None)?;
-    package_json.parse_package_manager()
-}
-
-fn get_command_args_creator(
-    package_manager: &PackageManager,
-) -> Result<Box<dyn CommandArgsCreatorTrait>, SnmError> {
-    let x: Box<dyn CommandArgsCreatorTrait> = match package_manager.name.as_str() {
-        "npm" => Box::new(NpmArgsTransform {}),
-        "pnpm" => Box::new(PnpmArgsTransform {}),
-        "yarn" => {
-            if get_is_less_2(&package_manager.version)? {
-                Box::new(YarnArgsTransform {})
-            } else {
-                Box::new(YarnPkgArgsTransform {})
-            }
-        }
-        _ => panic!("Unsupported package manager"),
-    };
-    Ok(x)
 }
 
 async fn get_manage(package_manager: &PackageManager) -> Result<Box<dyn ManageTrait>, SnmError> {
