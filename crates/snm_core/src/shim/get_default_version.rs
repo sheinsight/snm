c9f117c678fc3dcbbd43e8c1d1727c3425167151
ityuany <519495771@qq.com>
2024-06-18 22:01:39 +0800

    add read_runtime_dir_name_vec

--- crates/snm_core/src/shim/get_default_version.rs
+++ crates/snm_core/src/shim/get_default_version.rs
@@ -3,8 +3,6 @@ use snm_utils::snm_error::SnmError;
 
 use crate::traits::atom::AtomTrait;
 
-use super::get_runtime_dir_name_vec::read_runtime_dir_name_vec;
-
 pub fn get_default_version(
     shim: &dyn AtomTrait,
     snm_config: SnmConfig,
@@ -12,7 +10,7 @@ pub fn get_default_version(
     if snm_config.get_strict() {
         return Err(SnmError::NotFoundValidVersion);
     } else {
-        let (_, default_v) = read_runtime_dir_name_vec(shim)?;
+        let (_, default_v) = shim.read_runtime_dir_name_vec()?;
         default_v.ok_or(SnmError::NotFoundValidVersion)
     }
 }
--- crates/snm_core/src/traits/atom.rs
+++ crates/snm_core/src/traits/atom.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, pin::Pin};
+use std::{fs, ops::Not as _, path::PathBuf, pin::Pin};
 
 use futures_util::Future;
 use snm_utils::snm_error::SnmError;
@@ -64,4 +64,33 @@ pub trait AtomTrait {
     ) -> Result<PathBuf, SnmError>;
 
     fn get_anchor_file_path_buf(&self, v: &str) -> Result<PathBuf, SnmError>;
+
+    fn read_runtime_dir_name_vec(&self) -> Result<(Vec<String>, Option<String>), SnmError> {
+        let runtime_dir_path_buf = self.get_runtime_base_dir_path_buf()?;
+
+        let mut default_dir = None;
+
+        if runtime_dir_path_buf.exists().not() {
+            // TODO here create not suitable , should be find a better way
+            fs::create_dir_all(&runtime_dir_path_buf)?;
+        }
+
+        let dir_name_vec = runtime_dir_path_buf
+            .read_dir()?
+            .filter_map(|dir_entry| dir_entry.ok())
+            .filter(|dir_entry| dir_entry.path().is_dir())
+            .filter_map(|dir_entry| {
+                let file_name = dir_entry.file_name().into_string().ok()?;
+
+                if file_name.ends_with("-default") {
+                    default_dir = Some(file_name.trim_end_matches("-default").to_string());
+                    return None;
+                }
+
+                return Some(file_name);
+            })
+            .collect::<Vec<String>>();
+
+        Ok((dir_name_vec, default_dir))
+    }
 }
