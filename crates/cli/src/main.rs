56f742eac9f1dd2c9b9428f26ed85ca6a8611a8b
ityuany <519495771@qq.com>
2024-06-19 22:00:47 +0800

    review error handler

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,12 +1,18 @@
 use clap::Parser;
+use cli::{execute_cli::execute_cli, SnmCli};
 use snm_config::parse_snm_config;
 use snm_core::color_backtrace;
-
-use cli::{execute_cli::execute_cli, SnmCli};
 use snm_current_dir::current_dir;
+use snm_utils::snm_error::{friendly_error_message, SnmError};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
+    if let Err(error) = run().await {
+        friendly_error_message(error);
+    }
+}
+
+async fn run() -> Result<(), SnmError> {
     color_backtrace::install();
 
     let dir = current_dir()?;
@@ -15,7 +21,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let cli = SnmCli::parse();
 
-    execute_cli(cli, snm_config).await;
+    execute_cli(cli, snm_config).await?;
 
     Ok(())
 }
--- crates/snm_utils/src/snm_error.rs
+++ crates/snm_utils/src/snm_error.rs
@@ -1,5 +1,5 @@
+use colored::*;
 use std::path::PathBuf;
-
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -28,19 +28,8 @@ pub enum SnmError {
     #[error("Get workspace dir error")]
     GetWorkspaceError,
 
-    #[error("Not found node version file")]
-    NotFoundNodeVersionConfigFile,
-
     #[error("Not found valid version")]
     NotFoundValidVersion,
-
-    #[error(
-        "Not found default node version , please use `snm node use` to set default node version"
-    )]
-    NotFoundDefaultNodeVersion,
-
-    #[error("Not found package manager")]
-    NotFoundPackageManager,
 
     #[error("File already exists {0}")]
     FileAlreadyExists(PathBuf),
@@ -50,4 +39,164 @@ pub enum SnmError {
 
     #[error("Parse package manager error , raw is {0}")]
     ParsePackageManagerError(String),
+}
+
+pub fn friendly_error_message(error: SnmError) {
+    match error {
+        SnmError::BuildConfigError(_) => {
+            eprintln!(
+                r##"
+        👹 Build snm config error
+
+        The following is a list of configurations supported by snm:
+
+        SNM_STRICT: 
+
+            Whether to enable strict mode, default is false.
+            In strict mode, 
+            Must be a .node-version file in current_dir and the correct version number configured.
+            Must be a package.json in current_dir with the correct configuration of packageManager, for example: npm@8.0.0
+
+        SNM_NODE_BIN_DIR: 
+
+            The directory where the node binary is stored, default is node_bin.
+
+        SNM_DOWNLOAD_DIR: 
+
+            The directory where the downloaded file is stored, default is downloads.
+
+        SNM_NODE_MODULES_DIR: 
+
+            The directory where the node_modules is stored, default is node_modules.
+
+        SNM_NODE_DIST_URL: 
+
+            The download address of the node binary, the default is https://nodejs.org/dist .
+
+        SNM_GITHUB_RESOURCE_HOST: 
+
+            The download address of the node binary, the default is https://raw.githubusercontent.com .
+
+        SNM_NODE_INSTALL_STRATEGY: 
+
+            The installation strategy of the node binary, the default is auto. You can choose ask, panic, auto.
+
+        SNM_DOWNLOAD_TIMEOUT_SECS: 
+
+            The download timeout time, the default is 60s.
+
+        SNM_PACKAGE_MANAGER_INSTALL_STRATEGY: 
+
+            The installation strategy of the package manager, the default is auto. You can choose ask, panic, auto.
+
+            "##
+            );
+        }
+        SnmError::ParsePackageManagerError(raw) => {
+            eprintln!(
+                r##"
+        👹 Parse packageManager Error
+
+        The packageManager {} configured in your package.json is not being parsed correctly. 
+            "##,
+                raw.bold().red()
+            );
+        }
+        SnmError::ExceededMaxRetries(url) => {
+            eprintln!(
+                r##"
+        👹 Exceeded max retries
+
+        The download failed after {} retries. 
+            "##,
+                url.to_string().bold().red()
+            );
+        }
+        SnmError::NotFoundResourceError(url) => {
+            eprintln!(
+                r##"
+        👹 Not found resource
+
+        The resource {} was not found. 
+            "##,
+                url.to_string().bold().red()
+            );
+        }
+        SnmError::GetHomeDirError => {
+            eprintln!(
+                r##"
+        👹 Get home dir failed
+
+        I think the possible reasons are:
+
+        · The HOME environment variable is not set.
+        · The HOME environment variable is not set correctly.
+        · The HOME environment variable is not set to a directory.
+
+        Platform	    Value	                Example
+        Linux	        $HOME	                /home/alice
+        macOS	        $HOME	                /Users/Alice
+        Windows	        FOLDERID_Profile	C:\Users\Alice
+
+        Linux and macOS:
+        Use $HOME if it is set and not empty.
+        If $HOME is not set or empty, then the function getpwuid_r is used to determine the home directory of the current user.
+        If getpwuid_r lacks an entry for the current user id or the home directory field is empty, then the function returns None.
+        Windows:
+        This function retrieves the user profile folder using SHGetKnownFolderPath.
+
+        All the examples on this page mentioning $HOME use this behavior.
+
+        Note: This function's behavior differs from std::env::home_dir, which works incorrectly on Linux, macOS and Windows.
+            "##
+            );
+        }
+        SnmError::FileAlreadyExists(path_buf) => {
+            eprintln!(
+                r##"
+        👹 File already exists
+
+        The file {} already exists. 
+            "##,
+                path_buf.to_string_lossy().bold().red()
+            );
+        }
+        SnmError::NotFoundValidVersion => {
+            eprintln!(
+                r##"
+        👹 Not found valid version
+
+        If in strict mode, 
+
+        You must to be a .node-version file in current_dir and the correct version number configured.
+
+        You must to be a package.json in current_dir with the correct configuration of packageManager, for example: npm@8.0.0
+
+        If not in strict mode,
+
+        You can continue to follow strict mode rules 
+
+        or 
+
+        Use snm node default x.x.x to install the default version of node.
+
+        Use snm npm default x.x.x to install the default version of npm.
+
+        Use snm yarn default x.x.x to install the default version of yarn.
+
+        Use snm node install x.x.x to install the specified version of node.
+
+        You ensure that the default version exits at least one .
+        
+            "##
+            );
+        }
+        SnmError::HttpStatusCodeUnOk
+        | SnmError::GetWorkspaceError
+        | SnmError::DeserializeError(_)
+        | SnmError::NetworkError(_)
+        | SnmError::IOError(_) => {
+            panic!("{error}");
+        }
+    }
 }
