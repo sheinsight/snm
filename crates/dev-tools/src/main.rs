6bae0517371566fbce463e0a7243a6c07f580bd4
ityuany <519495771@qq.com>
2024-06-29 02:39:05 +0800

    chore: bump version to 0.0.1-89

--- crates/dev-tools/src/main.rs
+++ crates/dev-tools/src/main.rs
@@ -93,7 +93,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     run_git_command(&["add", "."]);
     run_git_command(&["commit", "-m", &format!("chore: bump version to {}", ans)]);
     run_git_command(&["tag", &format!("v{}", ans)]);
-    run_git_command(&["push"]);
+    // run_git_command(&["push"]);
+
+    println!("Please run `git push --tags` to push the tags to the remote repository.");
 
     Ok(())
 }
