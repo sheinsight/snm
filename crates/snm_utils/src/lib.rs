9046f5531332a2312fab2d086715283645ff97f8
ityuany <519495771@qq.com>
2024-07-07 03:11:00 +0800

    bugfix

--- crates/snm_utils/src/lib.rs
+++ crates/snm_utils/src/lib.rs
@@ -1,7 +1,7 @@
+pub mod check_duplicate_lock_file;
 pub mod color_backtrace;
 pub mod exec;
-pub mod health;
 pub mod snm_error;
 pub mod to_ok;
 
-pub use health::check_duplicate_lock_file;
+pub use check_duplicate_lock_file::check_duplicate_lock_file;
