08bf63f2737d7632425067c136c9ed003009b3b5
ityuany <519495771@qq.com>
2024-07-07 02:23:10 +0800

    bugfix

--- crates/snm_node/src/conditional_compiler.rs
+++ crates/snm_node/src/conditional_compiler.rs
@@ -1,64 +1,29 @@
-#[cfg(target_os = "windows")]
 pub fn get_tarball_ext() -> String {
-    "zip".to_string()
+    match std::env::consts::OS {
+        "windows" => "zip".to_string(),
+        "linux" => "tar.xz".to_string(),
+        "macos" => "tar.xz".to_string(),
+        _ => "unknown".to_string(), // 默认情况
+    }
 }
 
-#[cfg(target_os = "linux")]
-pub fn get_tarball_ext() -> String {
-    "tar.xz".to_string()
-}
-
-#[cfg(target_os = "macos")]
-pub fn get_tarball_ext() -> String {
-    "tar.xz".to_string()
-}
-
-#[cfg(target_arch = "x86")]
 pub fn get_arch() -> String {
-    "x86".to_string()
+    match std::env::consts::ARCH {
+        "x86" => "x86".to_string(),
+        "x86_64" => "x64".to_string(),
+        "arm" => "armv7l".to_string(),
+        "aarch64" => "arm64".to_string(),
+        "powerpc64" => "ppc64".to_string(),
+        "powerpc64le" => "ppc64le".to_string(),
+        "s390x" => "s390x".to_string(),
+        _ => "unknown".to_string(),
+    }
 }
 
-#[cfg(target_arch = "x86_64")]
-pub fn get_arch() -> String {
-    "x64".to_string()
-}
-
-#[cfg(target_arch = "arm")]
-pub fn get_arch() -> String {
-    "armv7l".to_string()
-}
-
-#[cfg(target_arch = "aarch64")]
-pub fn get_arch() -> String {
-    "arm64".to_string()
-}
-
-#[cfg(target_arch = "ppc64")]
-pub fn get_arch() -> String {
-    "ppc64".to_string()
-}
-
-#[cfg(target_arch = "ppc64le")]
-pub fn get_arch() -> String {
-    "ppc64le".to_string()
-}
-
-#[cfg(target_arch = "s390x")]
-pub fn get_arch() -> String {
-    "s390x".to_string()
-}
-
-#[cfg(target_os = "macos")]
 pub fn get_os() -> String {
-    "darwin".to_string()
-}
-
-#[cfg(target_os = "windows")]
-pub fn get_os() -> String {
-    "win".to_string()
-}
-
-#[cfg(not(target_os = "macos"))]
-pub fn get_os() -> String {
-    std::env::consts::OS.to_string()
+    match std::env::consts::OS {
+        "macos" => "darwin".to_string(),
+        "windows" => "win".to_string(),
+        _ => std::env::consts::OS.to_string(),
+    }
 }
