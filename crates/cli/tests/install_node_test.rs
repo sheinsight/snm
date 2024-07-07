50288070f12b44004be6e0723cb03bdf54aaf02a
ityuany <519495771@qq.com>
2024-07-08 00:43:54 +0800

    add set default test

--- crates/cli/tests/install_node_test.rs
+++ crates/cli/tests/install_node_test.rs
@@ -20,6 +20,7 @@ fn test_auto_install_node() -> Result<(), Box<dyn Error>> {
         .assert()
         .success()
         .stdout(predicate::str::contains(node_version));
+
     Ok(())
 }
 
@@ -54,6 +55,33 @@ fn test_delete_node() -> Result<(), Box<dyn Error>> {
         .assert()
         .success()
         .stdout(predicate::str::contains(node_version).not());
+
+    Ok(())
+}
+
+#[test]
+fn test_set_default_node() -> Result<(), Box<dyn Error>> {
+    let node_version = "20.12.1";
+    Command::cargo_bin("snm")?
+        .env("SNM_NODE_INSTALL_STRATEGY", "auto")
+        .arg("node")
+        .arg("install")
+        .arg(node_version)
+        .assert()
+        .success();
+
+    Command::cargo_bin("snm")?
+        .arg("node")
+        .arg("default")
+        .arg(node_version)
+        .assert()
+        .success();
+
+    Command::cargo_bin("node")?
+        .arg("-v")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(node_version));
 
     Ok(())
 }
