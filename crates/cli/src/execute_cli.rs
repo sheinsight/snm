dee0af9fa6d3b983b703fe2009a6f2fe7e5ece73
ityuany <519495771@qq.com>
2024-07-25 14:50:01 +0800

    review snm impl

--- crates/cli/src/execute_cli.rs
+++ crates/cli/src/execute_cli.rs
@@ -2,14 +2,40 @@ use snm_atom::node_atom::NodeAtom;
 use snm_config::SnmConfig;
 use snm_ni::trait_transform::IArgs;
 use snm_ni::{CommandArgsCreatorTrait, NpmArgsTransform, PnpmArgsTransform, YarnArgsTransform};
+use snm_utils::exec::exec_cli;
 use snm_utils::snm_error::SnmError;
-use std::process::{Command, Stdio};
 
 use crate::fig::fig_spec_impl;
 use crate::manage_command::ManageCommands;
 use crate::node_manager::node_manager::{ListArgs, ListRemoteArgs, NodeManager};
 use crate::snm_command::SnmCommands;
 use crate::SnmCli;
+
+fn create_transform(name: &str, version: &str) -> Box<dyn CommandArgsCreatorTrait> {
+    match name {
+        "npm" => Box::new(NpmArgsTransform {}),
+        "pnpm" => Box::new(PnpmArgsTransform {}),
+        "yarn" => Box::new(YarnArgsTransform {
+            version: version.to_string(),
+        }),
+        _ => panic!("Unsupported package manager: {}", name),
+    }
+}
+
+fn handle_command(transform: &dyn CommandArgsCreatorTrait, command: SnmCommands) -> Vec<String> {
+    match command {
+        // snm command start
+        SnmCommands::I(args) => transform.i(args),
+        SnmCommands::C(_) => transform.i(IArgs {
+            frozen_lockfile: true,
+        }),
+        SnmCommands::A(args) => transform.a(args),
+        SnmCommands::X(args) => transform.x(args),
+        SnmCommands::E(args) => transform.e(args),
+        SnmCommands::R(args) => transform.r(args),
+        _ => unreachable!("unreachable"),
+    }
+}
 
 pub async fn execute_cli(cli: SnmCli, snm_config: SnmConfig) -> Result<(), SnmError> {
     match cli.command {
@@ -50,43 +76,11 @@ pub async fn execute_cli(cli: SnmCli, snm_config: SnmConfig) -> Result<(), SnmEr
                 .package_manager
                 .ok_or(SnmError::NotFondPackageManagerConfigError {})?;
 
-            let transform: Box<dyn CommandArgsCreatorTrait> = match package_manager.name.as_str() {
-                "npm" => Box::new(NpmArgsTransform {}),
-                "pnpm" => Box::new(PnpmArgsTransform {}),
-                "yarn" => Box::new(YarnArgsTransform {
-                    version: package_manager.version.to_string(),
-                }),
-                _ => panic!("Unsupported package manager: {}", &package_manager.name),
-            };
+            let transform = create_transform(&package_manager.name, &package_manager.version);
 
-            let args = match cli.command {
-                // snm command start
-                SnmCommands::I(args) => transform.i(args),
-                SnmCommands::C(_) => transform.i(IArgs {
-                    frozen_lockfile: true,
-                }),
-                SnmCommands::A(args) => transform.a(args),
-                SnmCommands::X(args) => transform.x(args),
-                SnmCommands::E(args) => transform.e(args),
-                SnmCommands::R(args) => transform.r(args),
-                _ => unreachable!("unreachable"),
-            };
+            let args = handle_command(&*transform.as_ref(), cli.command);
 
-            let output = Command::new(package_manager.name)
-                .args(args)
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .stdin(Stdio::inherit())
-                .spawn()
-                .and_then(|process| process.wait_with_output())?;
-
-            if !output.status.success() {
-                return Err(SnmError::SNMBinaryProxyFail {
-                    stderr: String::from_utf8_lossy(&output.stderr).to_string(),
-                });
-            }
-
-            print!("{}", String::from_utf8_lossy(&output.stdout).to_string());
+            exec_cli(vec![], package_manager.name, args)?;
         }
 
         SnmCommands::FigSpec => {
