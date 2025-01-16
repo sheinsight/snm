4d4b0d9e78dbe61b940950673b9c35a2eb37f1d7
ityuany <519495771@qq.com>
2025-01-17 01:40:35 +0800

    临时 debug

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,32 +1,41 @@
-use std::{env::current_dir, process::ExitCode};
+use std::env::current_dir;
 
 // use clap::Parser;
 // use snm::{execute_cli::execute_cli, SnmCli};
 use snm_config::SnmConfig;
 
 #[tokio::main]
-async fn main() -> ExitCode {
-  match run().await {
-    Ok(_) => ExitCode::SUCCESS,
-    Err(e) => {
-      eprintln!("Error: {}", e);
-      ExitCode::FAILURE
-    }
-  }
-}
-
-async fn run() -> anyhow::Result<()> {
+async fn main() -> anyhow::Result<()> {
   let dir = current_dir()?;
 
   let snm_config = SnmConfig::from(dir)?;
-
-  // let cli = SnmCli::parse();
-
-  // execute_cli(cli, snm_config).await
 
   let nm = snm_node::factory::NodeFactory::new(&snm_config);
   nm.install(snm_node::factory::InstallArgs {
     version: "20.0.0".to_string(),
   })
   .await
+  // match run().await {
+  //   Ok(_) => ExitCode::SUCCESS,
+  //   Err(e) => {
+  //     eprintln!("Error: {}", e);
+  //     ExitCode::FAILURE
+  //   }
+  // }
 }
+
+// async fn run() -> anyhow::Result<()> {
+//   let dir = current_dir()?;
+
+//   let snm_config = SnmConfig::from(dir)?;
+
+//   // let cli = SnmCli::parse();
+
+//   // execute_cli(cli, snm_config).await
+
+//   let nm = snm_node::factory::NodeFactory::new(&snm_config);
+//   nm.install(snm_node::factory::InstallArgs {
+//     version: "20.0.0".to_string(),
+//   })
+//   .await
+// }
