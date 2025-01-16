b553453098b8890117a70ec4898e3dd8f5a6d40d
ityuany <519495771@qq.com>
2025-01-17 00:24:22 +0800

    demo

---
+++ e2e/tests/tmp_test.rs
@@ -0,0 +1,40 @@
+// #[tokio::test]
+// async fn test_reqwest_download() -> Result<(), Box<dyn std::error::Error>> {
+//   let mock_server = e2e::http_mocker::HttpMocker::builder()?.build().await?;
+
+//   let uri = mock_server.uri();
+
+//   let cur = std::env::current_dir()?;
+
+//   let config = snm_config::SnmConfig {
+//     node_bin_dir: cur.join(".snm").join("node_bin"),
+//     download_dir: cur.join(".snm").join("downloads"),
+//     cache_dir: cur.join(".snm").join("cache"),
+//     node_modules_dir: cur.join(".snm").join("node_modules"),
+//     node_dist_url: uri,
+//     node_github_resource_host: "https://raw.githubusercontent.com".to_string(),
+//     node_install_strategy: snm_config::InstallStrategy::Auto,
+//     node_white_list: "".to_string(),
+//     download_timeout_secs: 30,
+//     npm_registry: "https://registry.npmmirror.com".to_string(),
+//     workspace: cur.join("tests").join("fixtures").join("empty"),
+//     lang: "en".to_string(),
+//     restricted_list: vec![],
+//     strict: false,
+//   };
+
+//   let node_downloader = snm_node::downloader::NodeDownloader::new(&config);
+
+//   let res = node_downloader.download("20.0.0").await?;
+
+//   let node_bin_dir = config.node_bin_dir.join("20.0.0").join("bin").join("node");
+
+//   println!("res---->: {:?}", res);
+//   println!(
+//     "node_bin_dir---->: {:?} {}",
+//     node_bin_dir,
+//     node_bin_dir.exists()
+//   );
+
+//   Ok(())
+// }
