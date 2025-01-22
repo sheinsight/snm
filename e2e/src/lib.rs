123dd3808e301e91b191f111a5c85126ec16fff7
ityuany <519495771@qq.com>
2025-01-22 23:44:20 +0800

    debug

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -3,6 +3,7 @@ pub mod http_mocker;
 use std::path::PathBuf;
 use std::sync::Arc;
 
+use anyhow::Context;
 // use duct::cmd;
 use once_cell::sync::Lazy;
 use parking_lot::Mutex;
@@ -76,9 +77,9 @@ pub struct CommandBuilder {
 
 impl CommandBuilder {
   pub fn with_envs(name: &str, cwd: PathBuf, custom_envs: Vec<SnmEnv>) -> anyhow::Result<Self> {
-    let temp_dir = Builder::new()
+    let tmp_dir = Builder::new()
       .prefix(".snm")
-      .tempdir_in(dirs::home_dir()?)?
+      .tempdir_in(dirs::home_dir().context("Failed to get home dir")?)?
       .into_path();
     let env_path = env!("PATH");
 
