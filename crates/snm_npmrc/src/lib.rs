bb2b081c82ba47d9cf40ef49a0063716c8e2bbf7
ityuany <519495771@qq.com>
2025-02-02 02:43:49 +0800

    优化 npmrc 的读取逻辑

--- crates/snm_npmrc/src/lib.rs
+++ crates/snm_npmrc/src/lib.rs
@@ -3,7 +3,7 @@ use std::{
   path::{Path, PathBuf},
 };
 
-use config::{Config, File, FileFormat};
+use config::{Config, File, FileFormat, FileSourceFile};
 
 const DEFAULT_REGISTRY: &str = "https://registry.npmjs.org/";
 
@@ -18,64 +18,59 @@ pub struct NpmrcReader {
 
 impl NpmrcReader {
   pub fn from<P: AsRef<Path>>(workspace: P) -> Self {
-    let home_dir = match env::var_os("HOME") {
-      Some(home_dir) => home_dir,
-      None => return Self { config: None },
-    };
+    let sources = Self::collect_config_sources(workspace.as_ref());
 
-    let sources = {
-      #[cfg(target_os = "windows")]
-      {
-        // TODO: check if this is correct
-        vec![
-          env::var_os("APPDATA")
-            .map(PathBuf::from)
-            .map(|p| p.join(FILE_NAME))
-            .unwrap_or_default(),
-          PathBuf::from(&home_dir).join(FILE_NAME),
-          workspace.as_ref().to_path_buf().join(FILE_NAME),
-        ]
-      }
-      #[cfg(not(target_os = "windows"))]
-      {
-        let prefix = env::var_os("PREFIX").unwrap_or_default();
-        vec![
-          PathBuf::from("/")
-            .join(&prefix)
-            .join("etc")
-            .join(ETC_FILE_NAME),
-          PathBuf::from(&home_dir).join(FILE_NAME),
-          workspace.as_ref().to_path_buf().join(FILE_NAME),
-        ]
-      }
-    };
+    let config = Config::builder().add_source(sources).build().ok();
 
-    let sources = sources
+    Self { config }
+  }
+
+  fn collect_config_sources(workspace: &Path) -> Vec<File<FileSourceFile, FileFormat>> {
+    let mut sources = Vec::new();
+
+    // 添加系统特定路径
+    if let Some(sys_path) = Self::get_system_config_path() {
+      sources.push(sys_path);
+    }
+
+    // 添加通用路径
+    if let Some(home_dir) = dirs::home_dir() {
+      sources.push(home_dir.join(FILE_NAME));
+    }
+
+    sources.push(workspace.join(FILE_NAME));
+
+    sources
       .into_iter()
-      .filter_map(|path| {
-        path
-          .exists()
-          .then(|| File::from(path).format(FileFormat::Ini))
-      })
-      .collect::<Vec<_>>();
+      .filter(|path| path.exists())
+      .map(|path| File::from(path).format(FileFormat::Ini))
+      .collect()
+  }
 
-    Self {
-      config: Config::builder().add_source(sources).build().ok(),
+  fn get_system_config_path() -> Option<PathBuf> {
+    #[cfg(target_os = "windows")]
+    {
+      env::var_os("APPDATA").map(|app_data| PathBuf::from(app_data).join(FILE_NAME))
+    }
+    #[cfg(not(target_os = "windows"))]
+    {
+      env::var_os("PREFIX").map(|prefix| {
+        PathBuf::from("/")
+          .join(prefix)
+          .join("etc")
+          .join(ETC_FILE_NAME)
+      })
     }
   }
 
   pub fn read_registry_with_default(&self) -> String {
-    let registry = self
+    self
       .config
       .as_ref()
       .and_then(|c| c.get_string("registry").ok())
-      .unwrap_or(DEFAULT_REGISTRY.to_string());
-
-    if registry.ends_with('/') {
-      registry[..registry.len() - 1].to_string()
-    } else {
-      registry
-    }
+      .unwrap_or_else(|| DEFAULT_REGISTRY.to_string())
+      .trim_end_matches("/")
+      .to_string()
   }
 
   pub fn read(&self, key: &str) -> Option<String> {
