b5d659c90fef9e8a883e5f384681689b9b5e4ca2
ityuany <519495771@qq.com>
2025-02-04 03:52:36 +0800

    rename snode.get_bin to snode.ensure_node_and_return_dir

--- crates/snm_shim/src/node_shim.rs
+++ crates/snm_shim/src/node_shim.rs
@@ -5,7 +5,7 @@ use snm_utils::exec::exec_cli;
 pub async fn load_node(config: &SnmConfig, args: &Vec<String>) -> anyhow::Result<()> {
   let node_version_reader = SNode::try_from(&config)?;
 
-  let node_bin_dir = node_version_reader.get_bin().await?;
+  let node_bin_dir = node_version_reader.ensure_node_and_return_dir().await?;
 
   exec_cli(
     args,
