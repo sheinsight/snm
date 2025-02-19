1209a4d9cad3889bb36f6c22a60f67bf3de1ce46
ityuany <519495771@qq.com>
2025-02-19 11:34:55 +0800

    test install shell

--- install.sh
+++ install.sh
@@ -2,24 +2,13 @@
 set -e
 
 RELEASE="latest"
-FORCE_INSTALL="true"
 OS="$(uname -s)"
-
-if [ -d "$HOME/.snm" ]; then
-  INSTALL_DIR="$HOME/.snm"
-elif [ -n "$XDG_DATA_HOME" ]; then
-  INSTALL_DIR="$XDG_DATA_HOME/snm"
-elif [ "$OS" = "Darwin" ]; then
-  INSTALL_DIR="$HOME/Library/Application Support/snm"
-else
-  INSTALL_DIR="$HOME/.local/share/snm"
-fi
+INSTALL_DIR="$HOME/.snm"
 
 # Parse Flags
 parse_args() {
   while [[ $# -gt 0 ]]; do
     key="$1"
-
     case $key in
     -d | --install-dir)
       INSTALL_DIR="$2"
@@ -29,11 +18,6 @@ parse_args() {
     -s | --skip-shell)
       SKIP_SHELL="true"
       shift # past argument
-      ;;
-    --force-install | --force-no-brew)
-      echo "\`--force-install\`: I hope you know what you're doing." >&2
-      FORCE_INSTALL="true"
-      shift
       ;;
     -r | --release)
       RELEASE="$2"
@@ -49,68 +33,65 @@ parse_args() {
 }
 
 set_filename() {
-  
-  if [ "$OS" = "Linux" ]; then
-    # Based on https://stackoverflow.com/a/45125525
-    case "$(uname -m)" in
-      arm | armv7*)
-        echo "OS $OS is not supported."
-        exit 1
-        ;;
-      aarch* | armv8*)
-        FILENAME="x86_64-unknown-linux-musl.tar.gz"
-        ;;
-      *)
-        FILENAME="x86_64-unknown-linux-musl.tar.gz"
-    esac
-  elif [ "$OS" = "Darwin" ] && [ "$FORCE_INSTALL" = "true" ]; then
-    FILENAME="fnm-macos"
-    USE_HOMEBREW="false"
-    if [ "$(uname -m)" = "arm64" ]; then
-        FILENAME="aarch64-apple-darwin.tar.gz"
-    else
-        FILENAME="x86_64-apple-darwin.tar.gz"
-    fi
-    echo "Downloading the latest fnm binary from GitHub..."
-    # echo "  Pro tip: it's easier to use Homebrew for managing fnm in macOS."
-    # echo "           Remove the \`--force-no-brew\` so it will be easy to upgrade."
-  elif [ "$OS" = "Darwin" ]; then
-    # USE_HOMEBREW="true"
-    # echo "Downloading fnm using Homebrew..."
-    echo "not supported homebrew"
-    exit 1
-  else
+  # 首先处理不支持的情况
+  if [ "$OS" != "Linux" ] && [ "$OS" != "Darwin" ]; then
     echo "OS $OS is not supported."
-    echo "If you think that's a bug - please file an issue to https://github.com/Schniz/fnm/issues"
+    echo "If you think that's a bug - please file an issue to https://github.com/sheinsight/snm/issues"
     exit 1
   fi
+
+  # 处理具体的系统和架构
+  case "$OS" in
+  "Linux")
+    case "$(uname -m)" in
+    arm | armv7*)
+      echo "OS $OS is not supported."
+      exit 1
+      ;;
+    *)
+      FILENAME="x86_64-unknown-linux-musl.tar.gz"
+      ;;
+    esac
+    ;;
+  "Darwin")
+    case "$(uname -m)" in
+    "arm64")
+      FILENAME="aarch64-apple-darwin.tar.gz"
+      ;;
+    *)
+      FILENAME="x86_64-apple-darwin.tar.gz"
+      ;;
+    esac
+    echo "Downloading the latest snm binary from GitHub..."
+    ;;
+  esac
 }
 
-download_snm(){
-    if [ "$RELEASE" = "latest" ]; then
-      URL="https://github.com/sheinsight/snm/releases/latest/download/$FILENAME"
-    else
-      if [[ $RELEASE != v* ]]; then
-        RELEASE="v$RELEASE"
-      fi
-      URL="https://github.com/sheinsight/snm/releases/download/$RELEASE/$FILENAME"
+download_snm() {
+  if [ "$RELEASE" = "latest" ]; then
+    URL="https://github.com/sheinsight/snm/releases/latest/download/$FILENAME"
+  else
+    if [[ $RELEASE != v* ]]; then
+      RELEASE="v$RELEASE"
     fi
-    DOWNLOAD_DIR=$(mktemp -d)
-    echo "Downloading $URL..."
-    mkdir -p "$INSTALL_DIR" &>/dev/null
-        if ! curl --progress-bar --fail -L "$URL" -o "$DOWNLOAD_DIR/$FILENAME"; then
-      echo "Download failed.  Check that the release/filename are correct."
-      exit 1
-    fi
-    # unzip -q "$DOWNLOAD_DIR/$FILENAME" -d "$DOWNLOAD_DIR"
-    tar -xzf "$DOWNLOAD_DIR/$FILENAME" -C "$DOWNLOAD_DIR"
+    URL="https://github.com/sheinsight/snm/releases/download/$RELEASE/$FILENAME"
+  fi
+  DOWNLOAD_DIR=$(mktemp -d)
+  echo "Downloading $URL..."
+  mkdir -p "$INSTALL_DIR" &>/dev/null
+  if ! curl --progress-bar --fail -L "$URL" -o "$DOWNLOAD_DIR/$FILENAME"; then
+    echo "Download failed.  Check that the release/filename are correct."
+    exit 1
+  fi
+  # unzip -q "$DOWNLOAD_DIR/$FILENAME" -d "$DOWNLOAD_DIR"
+  tar -xzf "$DOWNLOAD_DIR/$FILENAME" -C "$DOWNLOAD_DIR"
 
-    for file in "$DOWNLOAD_DIR"/*; do
-        chmod u+x "$file"
-        mv "$file" "$INSTALL_DIR"
-    done
+  for file in "$DOWNLOAD_DIR"/*; do
+    chmod u+x "$file"
+    mv "$file" "$INSTALL_DIR"
+  done
 
-    echo "Downloaded to $DOWNLOAD_DIR"
+  echo "Downloaded to $DOWNLOAD_DIR"
 }
 
 check_dependencies() {
@@ -132,16 +113,6 @@ check_dependencies() {
     SHOULD_EXIT="true"
   fi
 
-  if [ "$USE_HOMEBREW" = "true" ]; then
-    echo -n "Checking availability of Homebrew (brew)... "
-    if hash brew 2>/dev/null; then
-      echo "OK!"
-    else
-      echo "Missing!"
-      SHOULD_EXIT="true"
-    fi
-  fi
-
   if [ "$SHOULD_EXIT" = "true" ]; then
     echo "Not installing fnm due to missing dependencies."
     exit 1
@@ -157,53 +128,46 @@ ensure_containing_dir_exists() {
   fi
 }
 
-setup_shell(){
-    CURRENT_SHELL="$(basename "$SHELL")"
+setup_shell() {
+  CURRENT_SHELL="$(basename "$SHELL")"
 
-  if [ "$CURRENT_SHELL" = "zsh" ]; then
-    CONF_FILE=${ZDOTDIR:-$HOME}/.zshrc
-    ensure_containing_dir_exists "$CONF_FILE"
-    echo "Installing for Zsh. Appending the following to $CONF_FILE:"
-    echo ""
-    echo '  # snm'
-    echo '  export PATH="'"$INSTALL_DIR"':$PATH"'
-
-    echo '' >>$CONF_FILE
-    echo '# snm' >>$CONF_FILE
-    echo 'export PATH="'$INSTALL_DIR':$PATH"' >>$CONF_FILE
-
-  elif [ "$CURRENT_SHELL" = "bash" ]; then
-    if [ "$OS" = "Darwin" ]; then
-      CONF_FILE=$HOME/.profile
-    else
-      CONF_FILE=$HOME/.bashrc
-    fi
-    ensure_containing_dir_exists "$CONF_FILE"
-    echo "Installing for Bash. Appending the following to $CONF_FILE:"
-    echo ""
-    echo '  # snm'
-    echo '  export PATH="'"$INSTALL_DIR"':$PATH"'
-
-    echo '' >>$CONF_FILE
-    echo '# snm' >>$CONF_FILE
-    echo 'export PATH="'"$INSTALL_DIR"':$PATH"' >>$CONF_FILE
-
-  else
+  case "$CURRENT_SHELL" in
+  "zsh")
+    CONF_FILE="${ZDOTDIR:-$HOME}/.zshrc"
+    SHELL_NAME="Zsh"
+    ;;
+  "bash")
+    CONF_FILE="$HOME/$([ "$OS" = "Darwin" ] && echo ".profile" || echo ".bashrc")"
+    SHELL_NAME="Bash"
+    ;;
+  *)
     echo "Could not infer shell type. Please set up manually."
     exit 1
-  fi
+    ;;
+  esac
 
+  ensure_containing_dir_exists "$CONF_FILE"
+
+  echo "Installing for $SHELL_NAME. Appending the following to $CONF_FILE:"
   echo ""
-  echo "In order to apply the changes, open a new terminal or run the following command:"
-  echo ""
+  echo "  # snm"
+  echo "  export PATH=\"$INSTALL_DIR:\$PATH\""
+
+  {
+    echo ""
+    echo "# snm"
+    echo "export PATH=\"$INSTALL_DIR:\$PATH\""
+  } >>"$CONF_FILE"
+
+  echo -e "\nIn order to apply the changes, open a new terminal or run the following command:\n"
   echo "  source $CONF_FILE"
 }
-
 
 parse_args "$@"
 set_filename
 check_dependencies
 download_snm
+
 if [ "$SKIP_SHELL" != "true" ]; then
   setup_shell
 fi
