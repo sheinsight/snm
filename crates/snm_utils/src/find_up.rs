9104ecc063fce1047544acaea070e18cc4d1dfa2
ityuany <519495771@qq.com>
2025-03-26 20:21:18 +0800

    feat(snm_utils): 添加查找文件功能
    - 新增 FindUp 模块，可递归查找上级目录中指定名称的文件
    - 在 src/lib.rs 中导入并导出 FindUp 模块
    - 添加两个示例 .node-version 文件辅助测试

---
+++ crates/snm_utils/src/find_up.rs
@@ -0,0 +1,45 @@
+use std::path::{Path, PathBuf};
+
+pub struct FindUp<P: AsRef<Path>> {
+  pub cwd: P,
+}
+
+impl<P: AsRef<Path>> FindUp<P> {
+  pub fn new(cwd: P) -> Self {
+    Self { cwd }
+  }
+
+  pub fn find(&self, name: &str) -> anyhow::Result<Vec<PathBuf>> {
+    let mut res = vec![];
+    let mut cwd = self.cwd.as_ref();
+    let file = cwd.join(name);
+
+    if file.exists() {
+      res.push(file);
+    }
+
+    while let Some(dir) = cwd.parent() {
+      let file = dir.join(name);
+      if file.exists() {
+        res.push(file);
+      }
+      cwd = dir;
+    }
+    println!("file: {:?}", res);
+
+    Ok(res)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+
+  use super::*;
+
+  #[test]
+  fn test_find_up() {
+    let find_up = FindUp::new("./fixtures/a/b/c/d");
+    let res = find_up.find(".node-version").unwrap();
+    assert_eq!(res.len(), 2);
+  }
+}
--- crates/snm_utils/src/lib.rs
+++ crates/snm_utils/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod consts;
 pub mod download;
 pub mod exec;
+mod find_up;
 pub mod log;
 pub mod tarball;
 pub mod ver;
+pub use find_up::FindUp;
