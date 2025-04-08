66f5a5441a4928574329e4bebae3e486ea413151
ityuany <519495771@qq.com>
2025-04-08 16:32:00 +0800

    feat: 优化 NodeVersion 结构体及其解析方法
    - 将 NodeVersion 结构体的 file 字段类型从 String 修改为泛型 P，以支持更灵活的路径处理。
    - 更新 parse 方法，简化参数，直接接收文件路径，增强代码可读性和灵活性。
    - 修正错误处理逻辑，确保在版本格式不正确时提供准确的文件名信息。

--- crates/snm_utils/src/node_version.rs
+++ crates/snm_utils/src/node_version.rs
@@ -3,28 +3,30 @@ use std::path::Path;
 use anyhow::bail;
 use lazy_regex::regex;
 
-pub struct NodeVersion {
+pub struct NodeVersion<P: AsRef<Path>> {
   pub raw: String,
   pub val: String,
-  pub file: String,
+  pub file: P,
 }
 
-impl NodeVersion {
-  pub async fn parse<P: AsRef<Path>>(raw: String, file: P) -> anyhow::Result<Self> {
-    let raw = tokio::fs::read_to_string(file).await?;
+impl<P: AsRef<Path>> NodeVersion<P> {
+  pub async fn parse(file: P) -> anyhow::Result<Self> {
+    let file_name = file.as_ref();
+
+    let raw = tokio::fs::read_to_string(file_name).await?;
 
     let val = raw.trim().to_lowercase().trim_start_matches("v").to_owned();
 
     let r = regex!(r"^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$");
 
     if !r.is_match(&val) {
-      bail!("Invalid Node.js version format: {} in {:#?}", &val, file);
+      bail!(
+        "Invalid Node.js version format: {} in {:#?}",
+        &val,
+        file_name
+      );
     }
 
-    Ok(Self {
-      raw,
-      val,
-      file: file.as_ref().to_string_lossy().into_owned(),
-    })
+    Ok(Self { raw, val, file })
   }
 }
