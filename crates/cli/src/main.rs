5d0be6068083074fd1657d06f8b2716e018c78e8
ityuany <519495771@qq.com>
2025-04-14 19:18:24 +0800

    chore: 移除 snm_pm 相关配置和注释
    - 从 `Cargo.toml` 文件中删除 `snm_pm` 模块的引用和相关注释，确保工作区配置的整洁性。
    - 清理 `cli` 模块中的注释代码，提升代码可读性。

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -14,30 +14,3 @@ async fn main() -> anyhow::Result<()> {
 
   SnmCli::parse().exec().await
 }
-
-// async fn xx() -> anyhow::Result<()> {
-//   let args = snm_pm::ops::ops::InstallArgs {
-//     package_spec: vec![],
-//     frozen: true,
-//     save_prod: false,
-//     save_peer: false,
-//     save_dev: false,
-//     save_optional: false,
-//     save_exact: false,
-//   };
-
-//   SnmCli::from(snm_command::SnmCommands::Install(args))
-//     .exec()
-//     .await?;
-
-//   let args = snm_pm::ops::ops::RunArgs {
-//     command: "build".to_string(),
-//     passthrough_args: vec![],
-//   };
-
-//   SnmCli::from(snm_command::SnmCommands::Run(args))
-//     .exec()
-//     .await?;
-
-//   Ok(())
-// }
