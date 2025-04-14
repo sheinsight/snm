51d8767f9ba6f3ef0ec3f61fe32e0d958c5bdd9b
ityuany <519495771@qq.com>
2025-04-14 15:51:55 +0800

    chore: 添加 snm_downloader 依赖并重构 PackageManagerSetup
    - 在 `Cargo.toml` 中添加 `snm_downloader` 依赖项以支持资源下载功能。
    - 重构 `PackageManagerSetup` 结构体中的 `find_up_package_manager` 和 `ensure_package_manager` 方法，增强包管理器的查找和下载逻辑。
    - 确保代码整洁性和可读性，保持功能一致性。

--- crates/package_manager/src/setup.rs
+++ crates/package_manager/src/setup.rs
@@ -1,47 +1,64 @@
-// use std::str::FromStr;
+use std::{path::PathBuf, str::FromStr};
 
-// use package_json_parser::PackageJsonParser;
+use package_json_parser::PackageJsonParser;
 use snm_config::snm_config::SnmConfig;
-// use up_finder::UpFinder;
+use snm_downloader::{DownloadPackageManagerResource, download_resource};
+use up_finder::UpFinder;
 
-// use crate::PackageManager;
+use crate::PackageManager;
 
 pub struct PackageManagerSetup {
   pub config: SnmConfig,
 }
 
 impl PackageManagerSetup {
-  // fn find_up_package_manager(&self) -> anyhow::Result<Option<PackageManager>> {
+  pub fn find_up_package_manager(&self) -> anyhow::Result<Option<PackageManager>> {
+    let find_up = UpFinder::builder().cwd(&self.config.workspace).build();
 
-  //   let find_up = UpFinder::builder()
-  //     .cwd(&self.config.workspace) // 从当前目录开始
-  //     .build();
+    let files = find_up.find_up("package.json");
 
-  //   let files = find_up.find_up("package.json");
+    if files.is_empty() {
+      return Ok(None);
+    }
 
-  //   if files.is_empty() {
-  //     return Ok(None);
-  //   }
+    let Some(package_manager_raw) = files.iter().find_map(|item| {
+      let Ok(package_json) = PackageJsonParser::parse(item) else {
+        return None;
+      };
 
-  //   let Some(package_manager) = files.iter().find_map(|item| {
-  //     let Ok(package_json) = PackageJsonParser::parse(item) else {
-  //       return None;
-  //     };
+      let Some(raw) = package_json.package_manager else {
+        return None;
+      };
 
-  //     let Some(raw) = package_json.package_manager else {
-  //       return None;
-  //     };
+      Some(raw)
+    }) else {
+      return Ok(None);
+    };
 
-  //     let Some(package_manager) = PackageManager::from_str(&raw.0).ok() else {
-  //       return None;
-  //     };
+    let package_manager = PackageManager::from_str(&package_manager_raw.0)?;
 
-  //     Some(package_manager)
-  //   }) else {
-  //     Ok(None)
-  //   }
+    Ok(Some(package_manager))
+  }
 
-  //   Ok(None)
+  pub async fn ensure_package_manager(
+    &self,
+    package_manager: &PackageManager,
+  ) -> anyhow::Result<PathBuf> {
+    let mut dir = self
+      .config
+      .node_modules_dir
+      .join(package_manager.name())
+      .join(package_manager.version());
+    let file = dir.join("package.json");
+    if !file.try_exists()? {
+      let resource = DownloadPackageManagerResource::builder()
+        .config(&self.config)
+        .bin_name(package_manager.name().to_string())
+        .version(package_manager.version().to_string())
+        .build();
 
-  // }
+      dir = download_resource(resource).await?;
+    }
+    Ok(dir)
+  }
 }
