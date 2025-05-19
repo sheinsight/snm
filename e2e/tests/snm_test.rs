0a489f66909314c799334dcd94f3c7e470cf9577
ityuany <519495771@qq.com>
2025-05-19 20:28:59 +0800

    新增测试用例 test_snm_install_with_invalid_private_package_json，验证处理无效的私有 package.json 文件的逻辑，并添加相应的快照文件以记录输出结果。

--- e2e/tests/snm_test.rs
+++ e2e/tests/snm_test.rs
@@ -144,3 +144,19 @@ async fn test_snm_whoami(ctx: &mut SnmTestContext) -> anyhow::Result<()> {
   })?;
   Ok(())
 }
+
+#[test_context(SnmTestContext)]
+#[tokio::test]
+async fn test_snm_install_with_invalid_private_package_json(
+  ctx: &mut SnmTestContext,
+) -> anyhow::Result<()> {
+  let cwd = current_dir()?.join("tests/fixtures/invalid_package_json");
+  ctx.start_server().await?;
+  ctx.set_cwd(&cwd);
+  ctx.exec("snm setup", false)?;
+  ctx.add_snapshot("snm i")?;
+  ctx.assert_snapshots(|res| {
+    insta::assert_snapshot!(res);
+  })?;
+  Ok(())
+}
